4f2cfe678a6a64906fffc48d66de2ffb2b41aae9
himalaya0035 <61822515+himalaya0035@users.noreply.github.com>
2020-11-11 18:46:57 +0530

    Update main2.js

--- js/main2.js
+++ js/main2.js
@@ -6,7 +6,7 @@ function getLocation() {
     } else {
         alert("Geolocation is not supported by this browser.");
         getApiData(undefined,undefined,'New Delhi')
-        .then((returnedPromise) => console.log("promise resolved"))
+        .then(() => console.log("promise resolved"))
         .catch(err => console.log(err.message))
         // will include a function here which will show the weather for New Delhi (default value)
     }
@@ -16,7 +16,7 @@ function getLatLon(position) {
     lat = position.coords.latitude;
     lon = position.coords.longitude;
     getApiData(lat, lon)
-        .then((returnedPromise) => console.log("promise resolved"))
+        .then(() => console.log("promise resolved"))
         .catch(err => console.log(err.message))
 }
 
@@ -90,20 +90,22 @@ var input = document.getElementById("searchBox");
 input.addEventListener("keyup", function (event) {
     if ("Enter" === event.key) { // new way of doing this
       event.preventDefault();
-      
-    //   document.getElementById("searchIcon").click();
-      searchedValue = input.value;
+      searchCity(); // well done himalaya
+    }
+});
+function searchCity(){
+    searchedValue = input.value;
+    input.value = '';
       if (searchedValue == ''){
          alert('Please enter a valid Keyword!!');
       }
       else{
         getApiData(undefined,undefined,searchedValue)
-        .then((returnedPromise) => console.log("promise resolved"))
+        .then(() => console.log("promise resolved"))
         .catch(err => console.log(err.message))
       }
-    }
-});
-
+    
+}
 function fillFirstRow(data, callback) {
     if (data.cod != 200){
         alert("Result Not Found, try searching a different City \n or search like (placeName, district, state) ");
@@ -124,15 +126,104 @@ function fillFirstRow(data, callback) {
     }
 }
 function fillChart(data, callback) {
+    let labelForGraph = [];
+    let dataForGraph = [];
     console.log(data);
-    // for (var i =0; i<24; i++){
-    //     hour = extractTime(data.hourly[i].dt)
-    //     hourlyTemp = Math.ceil(data.hourly[i].temp);
-    //     labelForGraph.push(hour);
-    //     dataForGraph.push(hourlyTemp);
-    // }
-    // console.log(labelForGraph)
-    // console.log(dataForGraph)
+    for (var i =0; i<24; i++){
+        hour = extractTime(data.hourly[i].dt)
+        hourlyTemp = Math.ceil(data.hourly[i].temp);
+        labelForGraph.push(hour);
+        dataForGraph.push(hourlyTemp);
+    }
+    // chart.js code starts here 
+    var ctx = document.getElementById("myAreaChart");
+    var myLineChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: labelForGraph,
+          datasets: [{
+            label: "Temperature",
+            lineTension: 0.3,
+            backgroundColor: "rgba(123, 15, 155, 0.2)",
+            borderColor: "rgba(255, 255, 255, 1)",
+            pointRadius: 3,
+            pointBackgroundColor: "rgba(56, 54, 40,1)",
+            pointBorderColor: "rgba(56, 54, 40,1)",
+            pointHoverRadius: 3,
+            pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+            pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+            pointHitRadius: 10,
+            pointBorderWidth: 2,
+            data: dataForGraph,
+          }],
+        },
+        options: {
+          maintainAspectRatio: false,
+          layout: {
+            padding: {
+              left: 10,
+              right: 25,
+              top: 25,
+              bottom: 0
+            }
+          },
+          scales: {
+            xAxes: [{
+              time: {
+                unit: 'date'
+              },
+              gridLines: {
+                display: false,
+                drawBorder: false
+              },
+              ticks: {
+                maxTicksLimit: 7
+              }
+            }],
+            yAxes: [{
+              ticks: {
+                maxTicksLimit: 5,
+                padding: 10,
+                callback: function(value, index, values) {
+                  return   number_format(value);
+                }
+              },
+              gridLines: {
+                color: "rgb(234, 236, 244)",
+                zeroLineColor: "rgb(234, 236, 244)",
+                drawBorder: false,
+                borderDash: [2],
+                zeroLineBorderDash: [2]
+              }
+            }],
+          },
+          legend: {
+            display: false
+          },
+          tooltips: {
+            backgroundColor: "rgb(255,255,255)",
+            bodyFontColor: "#858796",
+            titleMarginBottom: 10,
+            titleFontColor: '#6e707e',
+            titleFontSize: 14,
+            borderColor: '#dddfeb',
+            borderWidth: 1,
+            xPadding: 15,
+            yPadding: 15,
+            displayColors: false,
+            intersect: false,
+            mode: 'index',
+            caretPadding: 10,
+            callbacks: {
+              label: function(tooltipItem, chart) {
+                var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+                return datasetLabel + ': ' + number_format(tooltipItem.yLabel) + '\xB0' + 'C';
+              }
+            }
+          }
+        }
+      });
+    // chart.js code ends here
     callback(data)
 }
 
@@ -154,7 +245,7 @@ function fillForecastData(data) {
             desc : data.daily[i].weather[0].description,
             icon : data.daily[i].weather[0].icon,
         }
-        rowContainer.innerHTML += ` <div class="rowContent2 myBtn" id="">
+        rowContainer.innerHTML += ` <div class="rowContent2 myBtn" id="${i}">
         <div class="dayAndMonth">
             <h5>${obj.day}</h5>
             <h6>${obj.desc}</h6>
@@ -168,6 +259,13 @@ function fillForecastData(data) {
         </div>
     </div>` 
     }
+    for (i = 0; i < btn.length; i++) {
+        btn[i].addEventListener('click', function (event) {
+          clickedBtn = event.target;
+          setModalElement(clickedBtn.id,data);
+          modal.style.display = "block";
+        })
+      }
     document.getElementById('pop').innerHTML = data.daily[0].pop + '%';
     document.getElementById('uv').innerHTML = data.current.uvi;
     // extractDayAndMonth(data.current.dt)
@@ -175,7 +273,7 @@ function fillForecastData(data) {
 
 function extractTime(unixcode) {
     let date = new Date(unixcode * 1000);
-    return toStandardTime(date.toString().slice(16, 21));
+    return date.toString().slice(16, 21);
 }
 function extractDayAndMonth(unixcode){
     let date = new Date(unixcode * 1000);
@@ -184,5 +282,76 @@ function extractDayAndMonth(unixcode){
 }
 function toStandardTime(militaryTime) {
     militaryTime = militaryTime.split(':');
-    return (militaryTime[0].charAt(0) == 1 && militaryTime[0].charAt(1) > 2) ? (militaryTime[0] - 12) + ':' + militaryTime[1]  + ' PM' : militaryTime.join(':') + ' AM'
+    if (militaryTime[0].charAt(0) == 1 && militaryTime[0].charAt(1) > 2) {
+      return (militaryTime[0] - 12) + ':' + militaryTime[1] + ' P.M.';
+    } else {
+      return militaryTime.join(':') + ' A.M.';
+    }
+ }
+ Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+ Chart.defaults.global.defaultFontColor = 'white';
+
+ // function for chart.js
+function number_format(number, decimals, dec_point, thousands_sep) {
+    number = (number + '').replace(',', '').replace(' ', '');
+    var n = !isFinite(+number) ? 0 : +number,
+      prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+      sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+      dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+      s = '',
+      toFixedFix = function(n, prec) {
+        var k = Math.pow(10, prec);
+        return '' + Math.round(n * k) / k;
+      };
+    s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+    if (s[0].length > 3) {
+      s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+    }
+    if ((s[1] || '').length < prec) {
+      s[1] = s[1] || '';
+      s[1] += new Array(prec - s[1].length + 1).join('0');
+    }
+    return s.join(dec);
+}
+
+var modal = document.getElementById("myModal");
+modalBody = modal.getElementsByClassName("modal-body")[0];
+var btn = document.getElementsByClassName("myBtn");
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function () {
+    modal.style.display = "none";
+  }
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+  function setModalElement(id, data){
+  console.log(id)
+  modalBody.innerHTML = `
+  <div class="modalDetail">
+  <h5>Sunrise</h5>
+  <h5>${extractTime(data.daily[id].sunrise)}</h5>
+</div>
+<div class="modalDetail">
+<h5>Sunset</h5>
+<h5>${extractTime(data.daily[id].sunset)}</h5>
+</div>
+<div class="modalDetail">
+<h5>UVI</h5>
+<h5>${data.daily[id].uvi}</h5>
+</div>
+<div class="modalDetail">
+<h5>Precipitation</h5>
+<h5>${data.daily[id].pop}%</h5>
+</div>
+<div class="modalDetail">
+<h5>Humidity</h5>
+<h5>${data.daily[id].humidity}%</h5>
+</div>
+<div class="modalDetail">
+<h5>Wind Speed</h5>
+<h5>${data.daily[id].wind_speed} m/s</h5>
+</div>
+  `
 }
